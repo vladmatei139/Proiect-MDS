cb352580a6c595f724cb12a2d2dd5731b2680840
Raluca Mindrut <raluca.mindrut@yahoo.com>
2017-05-08 17:29:53 +0300

    MVC PROJECT
    BETA

---
+++ Incercare/Incercare/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Incercare.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "HELLO.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Game()
+        {
+            return View();
+
+        }
+        public ActionResult Easy()
+        {
+            return View();
+        }
+
+        
+    }
+}
